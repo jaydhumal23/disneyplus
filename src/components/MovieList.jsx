20f30ca3d4caf64612b52dab54d27d92cfae460a
Jay Dhumal <jaydhumal27@gmail.com>
2025-08-24 23:50:12 +0530

    feat: added scroll button for movielists

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Globalapi from '../services/Globalapi'
 import MovieCard from './MovieCard'
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 function MovieList({ genreId }) {
     const [movielist, setMovielist] = useState([])
+    const elementRef = useRef();
     useEffect(() => {
         getMovieByGenreId()
     }, [])
@@ -12,20 +14,39 @@ function MovieList({ genreId }) {
             setMovielist(respo.data.results)
         })
     }
+    const slideRight = (element) => {
+        console.log(window.innerWidth)
+        element.scrollLeft += 500;
+    }
+    const slideLeft = (element) => {
+        console.log(window.innerWidth)
+        element.scrollLeft -= 500;
+    }
     return (
-        <div className='display flex gap-8 overflow-x-auto w-full scrollbar-hide scroll-smooth duration-100 transition-all p-4'>
-            {movielist.map((data, index) => {
-                return (
-                    <div key={index }>
+        <div>
+            <HiChevronLeft className='text-gray-500 absolute mx-[-8px] mt-[120px] left-0 cursor-pointer text-5xl max-md:hidden'
+                onClick={() => {
+                    slideLeft(elementRef.current)
+                }} />
+            <HiChevronRight className='text-gray-500 absolute mx-[-10px] right-0 mt-[120px] text-5xl cursor-pointer max-md:hidden'
+                onClick={() => {
+                    slideRight(elementRef.current)
+                }} />
+            <div className='display flex gap-8 overflow-x-auto w-full scrollbar-hide scroll-smooth duration-100 transition-all p-4' ref={elementRef}>
+                {movielist.map((data, index) => {
+                    return (
+                        <div key={index}>
 
-                     <MovieCard movie={data} />
-                    </div>
-            )
+                            <MovieCard movie={data} />
+                        </div>
+                    )
 
-            })}
+                })}
 
 
+            </div>
         </div>
+
     )
 }
 
