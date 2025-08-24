a3bb9d4fb1a0154f887d8a1fc0f6413548b18fc2
Jay Dhumal <jaydhumal27@gmail.com>
2025-08-24 17:32:18 +0530

    added  border animation and border

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -7,7 +7,7 @@ function GenreMovieList() {
     <div className='  '>
         
 {GenreList.genere.map((data,index)=>index<=4&&(
-    <div key={index} className='p-8  ' >
+    <div key={index} className='px-8 py-4  ' >
         <h2 className='text-2xl text-gray-200 font-bold'>{data.name}
         </h2>
         <MovieList genreId={data.id} />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function MovieCard({ movie }) {
     return (
-        <div className='w-50 max-md:w-70 overflow-x-hidden  rounded-lg  '>
-            <img src={Movie_Url+movie.poster_path} className='object-cover w-full h-full'/>
+        <div className='w-50 max-md:w-70 overflow-hidden  rounded-lg hover:border-3 hover:border-gray-400 transition-all duration-300 cursor-pointer shadow-2xl shadow-gray-700 '>
+            <img src={Movie_Url+movie.poster_path} className='object-cover w-full h-full hover:scale-108 transition-all duration-300'/>
 
         </div>
     )
